f6af01d34c6843a1c5b069ea81762542ec798976
G4N05 <159679069+G4N05@users.noreply.github.com>
2026-07-03 13:33:07 +0700

    fix

--- AutomaHubMenu/Menu.lua
+++ AutomaHubMenu/Menu.lua
@@ -2,14 +2,21 @@
 
 local Fluent = loadstring(game:HttpGet("https://github.com/StyearX/Fluent-Modded/releases/download/Fluent/FluentPro"))() :: any
 
--- ponytail: require, readfile, or HTTP fallback
+type ThemeType = {
+    Init: (Fluent: any, Tab: any) -> any
+}
+
 local Theme = (function()
-    local success, module = pcall(require, script.Parent:WaitForChild("Theme") :: ModuleScript)
+    local success, module = pcall(function()
+        local parent = script and script.Parent
+        if not parent then error("no parent") end
+        return require(parent:WaitForChild("Theme", 2) :: ModuleScript)
+    end)
     if success then return module end
     local ok, fileContent = pcall(readfile, "AutomaHub/AutomaHubMenu/Theme.lua")
     if ok then return loadstring(fileContent)() end
     return loadstring(game:HttpGet("https://raw.githubusercontent.com/G4N05/AutomaHub/main/AutomaHubMenu/Theme.lua"))()
-end)()
+end)() :: ThemeType
 
 local Window = Fluent:CreateWindow({
     Title = "GUI",
