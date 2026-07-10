8efd4c0612931381df4deacf9fc21c6b0f954b61
G4N05 <maospatirendy@gmail.com>
2026-07-10 21:08:40 +0700

    back

--- AutomaHubGui/Menu.lua
+++ AutomaHubGui/Menu.lua
@@ -158,16 +158,6 @@ VaultSection:Toggle({
         end
     end
 })
-VaultSection:Toggle({
-    Title = "Auto Vault",
-    Desc = "Automatically vault near windows",
-    Value = false,
-    Callback = function(value: boolean)
-        if Combat and Combat.SetAutoVault then
-            Combat.SetAutoVault(value)
-        end
-    end
-})
 
 local SkillcheckSection = CombatTab:Section({ Title = "Skillcheck Settings" })
 SkillcheckSection:Toggle({
