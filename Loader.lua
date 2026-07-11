ccb652b541242a4f01c3c0a79780ddc13d6209f3
G4N05 <maospatirendy@gmail.com>
2026-07-11 11:41:22 +0700

    ba

--- Loader.lua
+++ Loader.lua
@@ -6,7 +6,7 @@ getgenv().AutomaHubLoaderModule = true
 
 local function fetchScript(path: string): any
     local content
-    if isfile then
+    if _G.AutomaHubDeveloperMode and isfile then
         if isfile(path) then
             pcall(function() content = readfile(path) end)
         elseif isfile("AutomaHub/" .. path) then
